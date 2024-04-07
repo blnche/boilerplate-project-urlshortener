f4cf03237a05f39fb5ab0f834ef10dd05dcf77f8
blnche <peltierblanche@gmail.com>
2024-04-07 22:14:03 +0200

    url validator done

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const boydParser = require('body-parser');
+const dns = require('node:dns');
+
+app.use(boydParser.json());
+app.use(boydParser.urlencoded({extended: false}));
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 
@@ -17,6 +22,37 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req, res) => {
+  // const urlValidator = /(((http|https):\/\/)|(\/)|(..\/))(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  // console.log(req.body.url);
+
+  const url = req.body.url;
+
+  const cleanedUrl = url.replace(/^https?:\/\//, '');
+
+  dns.lookup(cleanedUrl, (err, address, family) => {
+    console.log('address: %j family: IPv%s', address, family);
+
+    if (!address) {
+      res.json({
+        error: 'invalid url'
+      })
+    } else {
+      res.json({
+        original_url: url,
+        short_url: ''
+      });
+    }
+  });
+
+  console.log('headers '+JSON.stringify(req.headers, null, 2))
+  console.log('body '+JSON.stringify(req.body, null, 2))
+
+  //next();
+//}, (req, res) => {
+
 });
 
 app.listen(port, function() {
